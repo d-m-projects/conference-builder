cfb309ae0b526df695987703407f2f7d6d3987d6
chip <chiposaur@gmail.com>
2020-08-04 15:10:15 -0500

    antd added, header styling blockout.

--- src/components/landing-page/components/Header.js
+++ src/components/landing-page/components/Header.js
@@ -1,21 +1,26 @@
 //Packages
 import React from "react";
 import { Link } from "react-router-dom";
+
+import './Header.css';
+
+
+
 //Components
 
 import Nav from "./Nav";
 
 function Header() {
-  return (
-    <div>
-      <div>
-        <Link to="/">
-          <h4>This is the Header!</h4>
-        </Link>
-      </div>
-      <Nav />
-    </div>
-  );
+	return (
+		<header>
+			<div>
+				<Link to="/">
+					<h4>This is the Header!</h4>
+				</Link>
+			</div>
+			<Nav />
+		</header>
+	);
 }
 
 export default Header;
--- src/components/landing-page/components/Nav.js
+++ src/components/landing-page/components/Nav.js
@@ -1,17 +1,27 @@
 //Packages
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { Menu } from "antd"
+import 'antd/dist/antd.css';
+import './Nav.css';
+
 //Components
 
 const Nav = () => {
-  return (
-    <div>
-      <Link exact to="/">
-        Home
-      </Link>
-      <Link to="/calendar">Calendar</Link>
-    </div>
-  );
+
+	const handleClick = () => { }
+
+	return (
+		<Menu onClick={handleClick} mode="horizontal">
+			<Menu.Item>
+				<Link exact to="/">Home</Link>
+			</Menu.Item>
+			<Menu.Item>
+				<Link to="/calendar">Calendar</Link>
+			</Menu.Item>
+		</Menu>
+	);
 };
 
 export default Nav;
