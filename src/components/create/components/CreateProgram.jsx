67922c21e1d1dae9b4e6557860fcc8965f170d16
Michael Nunes <tinysquid.git@gmail.com>
2020-08-06 18:01:47 -0700

    Add form for program creation, hooked up to context

--- src/components/create/components/CreateProgram.jsx
+++ src/components/create/components/CreateProgram.jsx
@@ -1,15 +1,47 @@
 import React, { useContext } from "react";
 import ProgramContext from "../../../contexts/programContext";
 
-// antd setup
-import "antd/dist/antd.css";
+// antd components
+import { Form, Input, Button, DatePicker } from "antd";
 
-const CreateProgram = () => {
+const { RangePicker } = DatePicker;
+
+const CreateProgram = ({ formNext }) => {
   const program = useContext(ProgramContext);
 
-  console.log("PROGRAM CONTEXT:\n", program);
+  const onFinish = (values) => {
+    console.log("Success:", values);
 
-  return <div>Creation instructions</div>;
+    program.Name = values.programName;
+    program.DateStart = values.programLength[0]._d;
+    program.DateEnd = values.programLength[1]._d;
+
+    formNext();
+  };
+
+  return (
+    <Form name="basic" onFinish={onFinish}>
+      <Form.Item
+        label="Program Name"
+        name="programName"
+        rules={[{ required: true, message: "Please input a valid name." }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Program Length"
+        name="programLength"
+        rules={[{ required: true, message: "Please select a valid date range." }]}>
+        <RangePicker />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" shape="round">
+          Create
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default CreateProgram;
