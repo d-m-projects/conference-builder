6c5f6c18552dfb411db0fb213b365d1fe2f2368a
chip <chiposaur@gmail.com>
2020-08-13 15:47:10 -0500

    .

--- src/components/file/index.js
+++ src/components/file/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const File = () => {
+	const DATA = {
+		name: "",
+		dateStart: null,
+		dateEnd: null,
+		days: [],
+		sessions: [],
+		nextSessionId: 0,
+		tempSession: {},
+	}
+
 	return (<div>This is the File Manager!</div>);
 };
 
