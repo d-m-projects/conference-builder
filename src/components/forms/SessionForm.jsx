05031ca810d4cd8ed903107211f05e797c133c30
Michael Nunes <tinysquid.git@gmail.com>
2020-08-19 11:28:59 -0700

    SessionForm hooked up to modal flow

--- src/components/forms/SessionForm.jsx
+++ src/components/forms/SessionForm.jsx
@@ -1,13 +1,19 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { ProgramContext } from "../../contexts/Program";
 
-import { Form, Input, DatePicker, Button } from "antd";
+import { Form, Input, DatePicker, Button, message } from "antd";
+
+import FlowSwitchOneModal from "../Modals/FlowSwitchOne/FlowSwitchOneModal";
 
 const { RangePicker } = DatePicker;
 
-function SessionForm() {
+function SessionForm(props) {
+  const { setFormView } = props;
+
   const program = useContext(ProgramContext);
   const { createSession } = program;
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   const [form] = Form.useForm();
 
@@ -22,43 +28,52 @@ function SessionForm() {
       presentations: [],
     });
 
+    message.success(`Session ${values.sessionName} created!`);
+
+    setTimeout(() => {
+      setModalVisible(true);
+    }, 200);
+
     form.resetFields();
   };
 
   return (
-    <div className="session-form-container">
-      <Form
-        form={form}
-        name="sessionForm"
-        className="session-form"
-        onFinish={formSubmit}
-        autoComplete="off"
-        layout="vertical"
-        hideRequiredMark>
-        {/* SESSION NAME */}
-        <Form.Item
-          label="Session Name"
-          name="sessionName"
-          rules={[{ required: true, message: "Input a name for this session." }]}>
-          <Input />
-        </Form.Item>
+    <>
+      <FlowSwitchOneModal isVisible={modalVisible} setVisibility={setModalVisible} setFormView={setFormView} />
+      <div className="session-form-container">
+        <Form
+          form={form}
+          name="sessionForm"
+          className="session-form"
+          onFinish={formSubmit}
+          autoComplete="off"
+          layout="vertical"
+          hideRequiredMark>
+          {/* SESSION NAME */}
+          <Form.Item
+            label="Session Name"
+            name="sessionName"
+            rules={[{ required: true, message: "Input a name for this session." }]}>
+            <Input />
+          </Form.Item>
 
-        {/* DATETIME RANGE */}
-        <Form.Item
-          label="Session Start & End Dates"
-          name="sessionLength"
-          rules={[{ required: true, message: "Input a date range for this session." }]}>
-          <RangePicker showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" />
-        </Form.Item>
+          {/* DATETIME RANGE */}
+          <Form.Item
+            label="Session Start & End Dates"
+            name="sessionLength"
+            rules={[{ required: true, message: "Input a date range for this session." }]}>
+            <RangePicker showTime={{ format: "HH:mm" }} format="YYYY-MM-DD HH:mm" />
+          </Form.Item>
 
-        {/* SUBMIT */}
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Create Session
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          {/* SUBMIT */}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Create Session
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   );
 }
 
