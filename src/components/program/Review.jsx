14da1be7247bf8c30b4103a217bb78a95a39e2b6
chip <chiposaur@gmail.com>
2020-08-27 17:34:51 -0500

    beaut dnd removed

--- src/components/program/Review.jsx
+++ src/components/program/Review.jsx
@@ -3,34 +3,13 @@ import { ProgramContext } from "../../contexts/Program";
 import { useLocation } from "react-router-dom";
 
 import moment from "moment";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 // antd components
 import { Skeleton, Card, List } from "antd";
 
 // Components
 import { VIEW } from "../forms/FormManager";
-
-const getDraggableItemStyle = (isDragging, draggableStyle) => ({
-	// some basic styles to make the items look a bit nicer
-	userSelect: "none",
-	padding: 16,
-	marginBottom: 4,
-
-	// // change background colour if dragging
-	// background: isDragging ? "lightgreen" : "white",
-	// boxShadow: "0 0 3px 1px rgba(40, 40, 40, 0.35)",
-	border: "1px solid #ddd",
-
-	// // styles we need to apply on draggables
-	...draggableStyle,
-});
-
-// This is the style for the container that holds the draggables
-const getListStyle = () => ({
-	// padding: 8,
-	width: "100%",
-});
 
 const Review = (props) => {
 	// Top level of the Review
@@ -53,82 +32,18 @@ const Review = (props) => {
 	// 	program.injectTestData()
 	// }
 
-	useEffect(() => {
-		setDays(program.days)
-	}, [program])
-
-	const reorderDays = (list, startIndex, endIndex) => {
-		// Swaps the days in the list
-		// and keeps the dates in the original order.
-
-		const result = Array.from(list)
-
-		let alldates = []
-		for (let i = 0; i < result.length; i++) {
-			alldates.push(result.slice(i, i + 1)[0].date)
-		}
-		
-		const [removed] = result.splice(startIndex, 1)
-		result.splice(endIndex, 0, removed)
-		for (let i = 0; i < result.length; i++) {
-			result[i].date = alldates[i]
-		}
-		return result
-	}
-
-	const onDragEnd = (e) => {
-		// Dropped outside the list
-		if (!e.destination) {
-			return;
-		}
-		const orderedDays = reorderDays(days, e.source.index, e.destination.index);
-		updateProgram({...program, days: orderedDays});
-	};
-
-	const programdata = (p) => {
-		p.programDateString = `Program Day: ${moment(p.date).format("ddd, MMM Do Y")}`
-		p.programToFrom = `${moment(p.dateStart).format("MMM DD")} - ${moment(p.dateEnd).format("MMM DD")}`
-		return p
-	}
-
 	return (
 		program.dateStart
-			? <Card title={program.name} extra={programdata(program).programToFrom} bodyStyle={{ display: "flex" }}>
-				<DragDropContext
-					// onBeforeCapture={onBeforeCapture}
-					// onBeforeDragStart={onBeforeDragStart}
-					// onDragStart={onDragStart}
-					// onDragUpdate={onDragUpdate}
-					onDragEnd={onDragEnd}
-				>
-					<Droppable droppableId="droppable">
-						{(provided, snapshot) => (
-							<div
-								{...provided.droppableProps}
-								ref={provided.innerRef}
-								style={getListStyle(snapshot.isDraggingOver)}
-							>
-								{program.days.map((day, index) => (
-									<Draggable key={day.date} draggableId={day.date} index={index}>
-										{(provided, snapshot) => (
-											<div className={`program-agenda`}
-												ref={provided.innerRef}
-												{...provided.draggableProps}
-												{...provided.dragHandleProps}
-												style={getDraggableItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-											>
-												<Sessions
-													props={{ sessions: day.sessions, dayHeader: programdata(day).programDateString }}
-												/>
-											</div>
-										)}
-									</Draggable>
-								))}
-								{provided.placeholder}
-							</div>
-						)}
-					</Droppable>
-				</DragDropContext>
+			? <Card title={program.name} extra="program dates here" bodyStyle={{ display: "flex" }}>
+				<div>
+					{program.days.map((day, index) => (
+						<div className={`program-agenda`}>
+							<Sessions
+								props={{ sessions: day.sessions, dayHeader: "single day date" }}
+							/>
+						</div>
+					))}
+				</div>
 			</Card>
 			: <Skeleton />
 	)
@@ -146,19 +61,9 @@ const Sessions = ({ props }) => {
 		<div>
 			<h3>{props.dayHeader}</h3>
 			{props.sessions.map((session, index) => (
-				// <Draggable key={session.name} draggableId={session.name} index={index}>
-				// 	{(provided, snapshot) => (
-				<div key={index} style={{ marginLeft: "20px" }}
-				// ref={provided.innerRef}
-				// {...provided.draggableProps}
-				// {...provided.dragHandleProps}
-				// style={getDraggableItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-				>
-					{/* <h3>Session: {session.name}</h3> */}
+				<div key={index} style={{ marginLeft: "20px" }}>
 					<Presentations props={{ pres: session.presentations, sessionHeader: sessiondata(session).sessionsDateString }} />
 				</div>
-				// 	)}
-				// </Draggable>
 			))}
 
 		</div>
@@ -172,19 +77,10 @@ const Presentations = ({ props }) => {
 		<div>
 			<h4>{props.sessionHeader}</h4>
 			{props.pres.map((pres, index) => (
-				// <Draggable key={pres.name} draggableId={pres.name} index={index}>
-				// 	{(provided, snapshot) => (
-				<div key={index} style={{ marginLeft: "20px" }}
-				// ref={provided.innerRef}
-				// {...provided.draggableProps}
-				// {...provided.dragHandleProps}
-				// 	style={getDraggableItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-				>
+				<div key={index} style={{ marginLeft: "20px" }}>
 					<h4 style={{ fontStyle: "oblique" }}>Presentation: {pres.name}</h4>
 					<p key={index}>By: {pres.presenters.join(", ")}</p>
 				</div>
-				// 	)}
-				// </Draggable>
 			))}
 		</div>
 	)
